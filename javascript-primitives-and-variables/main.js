f543be2f30f12404d6d47225420364c2bdc6a5f6
Viviane Vieira <viviane.kodama@gmail.com>
2020-10-12 13:16:51 -0700

    Completed javascript primitives and variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+var fullName = 'Sarah Lee';
+var isCool = true;
+var totalPets = 52;
+
+console.log('Full Name:', fullName);
+console.log('Is cool:', isCool);
+console.log('Total pets:', totalPets);
+
+console.log('Type of fullName:', typeof fullName);
+console.log('Type of isCool:', typeof isCool);
+console.log('Type of totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('Value of unknown:', unknown);
+console.log('Type of unknown:', typeof unknown);
+
+var never;
+console.log('Value of never:', never);
+console.log('Type of never:', typeof never);
