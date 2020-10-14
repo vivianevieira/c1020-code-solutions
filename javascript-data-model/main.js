77a31cdc253d7ef654b32787e2d6713f877d1c85
Viviane Vieira <viviane.kodama@gmail.com>
2020-10-13 19:46:32 -0700

    Change request javascript data model.

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -2,23 +2,68 @@
 
 var orderHistory = [
   {
-    order1Header: [{ orderNo: '114-3941689-8772232' }, { datePlaced: 'August 4, 2020' }, { total: 34 }, { shipTo: 'JS Masher' }, { delivered: '08/08/2020' }],
-    order1Item1: [{ type: 'book', title: 'JavaScript for impatient programmers', author: 'Dr. Axel Rauschmayer', returnWindow: '09/07/2020', price: 31.55 }]
+    orderNo: '114-3941689-8772232',
+    datePlaced: '08/04/2020',
+    total: 34,
+    shipTo: 'JS Masher',
+    delivered: '08/08/2020',
+    items: [
+      {
+        type: 'book',
+        title: 'JavaScript for impatient programmers',
+        author: 'Dr. Axel Rauschmayer',
+        returnWindow: '09/07/2020',
+        price: 31.55
+      }
+    ]
   },
-
   {
-    order2Header: [{ orderNo: '113-9984268-1280257' }, { datePlaced: 'July 19, 2020' }, { total: 44.53 }, { shipTo: 'JS Masher' }, { delivered: '08/20/2020' }],
-    order2Item1: [{ type: 'book', title: 'The Timeless Way of Building', price: 41.33, returnWindow: '08/19/2020' }]
+    orderNo: '113-9984268-1280257',
+    datePlaced: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    delivered: '08/20/2020',
+    items: [
+      {
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        price: 41.33,
+        returnWindow: '08/19/2020'
+      }
+    ]
   },
-
   {
-    order3Header: [{ orderNo: '114-28575557-' }, { datePlaced: 'July 4, 2020' }, { total: 17.22 }, { shipTo: 'JS Masher' }],
-    order3Item1: [{ type: 'gaming', title: 'Gamecube Controller Adapter', price: 15.98 }, { returnWindow: '08/05/2020' }]
+    orderNo: '114-28575557-9059409',
+    datePlaced: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    items: [
+      {
+        type: 'gaming',
+        title: 'Gamecube Controller Adapter',
+        price: 15.98,
+        returnWindow: '08/05/2020'
+      }
+    ]
   },
-
   {
-    order4Header: [{ orderNo: '113-2883177-2648248' }, { datePlaced: 'July 3, 2020' }, { total: 138.93 }, { shipTo: 'JS Masher' }],
-    order4Item1: [{ type: 'gaming', title: 'GameCube Controller', price: 94.95, returnWindow: '08/04/2020' }, { type: 'book', title: 'The Art of Sql', price: 33.99, returnWindow: '08/04/2020' }]
+    orderNo: '113-2883177-2648248',
+    datePlaced: 'July 3, 2020',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    items: [
+      {
+        type: 'gaming',
+        title: 'GameCube Controller',
+        price: 94.95,
+        returnWindow: '08/04/2020'
+      },
+      {
+        type: 'book',
+        title: 'The Art of Sql',
+        price: 33.99,
+        returnWindow: '08/04/2020'
+      }
+    ]
   }
-
 ];
