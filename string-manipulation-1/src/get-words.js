9737466a34a3e2bbb8c11b956f12174b58213a88
Viviane Vieira <viviane.kodama@gmail.com>
2020-10-18 18:17:46 -0700

    Completed string manipulation 1 exercise.

---
+++ string-manipulation-1/src/get-words.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+function getWords(string) {
+  return string.split(' ');
+}
---
+++ string-manipulation-1/src/is-vowel.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+function isVowel(char) {
+  if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+      char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+    return true;
+  } else {
+    return false;
+  }
+}
