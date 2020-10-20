69a395ef93aff797184fc188973bfc28b6d1586a
Viviane Vieira <viviane.kodama@gmail.com>
2020-10-19 17:15:43 -0700

    Completed DOM manipulation exercise.

---
+++ dom-manipulation/main.js
@@ -0,0 +1,25 @@
+var counter = 1;
+
+var hotButton = document.querySelector('.hot-button');
+
+var clickcount = document.querySelector('.click-count');
+
+// hotButton.addEventListener('click', function () { counter++; console.log(counter); });
+
+hotButton.addEventListener('click', function () {
+  clickcount.textContent = 'Clicks: ' + counter++;
+  if (counter <= 4) {
+    hotButton.className = 'hot-button cold';
+  } else if (counter <= 7) {
+    hotButton.className = 'hot-button cool';
+  } else if (counter <= 10) {
+    hotButton.className = 'hot-button tepid';
+  } else if (counter <= 13) {
+    hotButton.className = 'hot-button warm';
+  } else if (counter <= 16) {
+    hotButton.className = 'hot-button hot';
+  } else {
+    hotButton.className = 'hot-button nuclear';
+  }
+
+});
