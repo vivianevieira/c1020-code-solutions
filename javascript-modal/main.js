7bb891a1c878471475996cde1f1dc3e2edd73637
Viviane Vieira <viviane.kodama@gmail.com>
2020-10-21 21:59:00 -0700

    Revised modal main.js file.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,19 +1,11 @@
-var modalOn = false;
-
 var $openButton = document.querySelector('.open-button');
 var $modal = document.querySelector('.modal');
 var $redButton = document.querySelector('.red-button');
 
 $openButton.addEventListener('click', function () {
-  modalOn = !modalOn;
-  if (modalOn) {
-    $modal.className = 'modal';
-  }
+  $modal.className = 'modal';
 });
 
 $redButton.addEventListener('click', function () {
-  modalOn = !modalOn;
-  if (modalOn === false) {
-    $modal.className = 'modal no-display';
-  }
+  $modal.className = 'modal no-display';
 });
