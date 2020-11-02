2e129c22597bb094e3f322fed9f47abc71f7a5b4
Viviane Vieira <viviane.kodama@gmail.com>
2020-11-02 15:11:05 -0800

    Revised file to remove commented out lines of code.

--- javascript-prototypes/warner-bros.js
+++ javascript-prototypes/warner-bros.js
@@ -4,33 +4,18 @@ var yakko = {
   age: 14,
   name: 'Yakko',
   role: 'brother'
-  // describe: function () {
-  //   var description = this.name + ' is a ' +
-  //                     this.age + '-year-old Warner ' + this.role + '.';
-  //   return description;
-  // }
 };
 
 var wakko = {
   age: 11,
   name: 'Wakko',
   role: 'brother'
-  // describe: function () {
-  //   var description = this.name + ' is a ' +
-  //                     this.age + '-year-old Warner ' + this.role + '.';
-  //   return description;
-  // }
 };
 
 var dot = {
   age: 10,
   name: 'Dot',
   role: 'sister'
-  // describe: function () {
-  //   var description = this.name + ' is a ' +
-  //                     this.age + '-year-old Warner ' + this.role + '.';
-  //   return description;
-  // }
 };
 
 var warnerPrototype = {
