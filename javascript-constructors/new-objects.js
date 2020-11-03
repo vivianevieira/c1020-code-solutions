33a048734550e32a6d83c5f2778cf9ec86627432
Viviane Vieira <viviane.kodama@gmail.com>
2020-11-03 11:19:56 -0800

    Completed Javascript constructors exercise.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,19 @@
+var anObject = new Object();
+console.log('value of anObject:', anObject);
+console.log('typeof anObject', typeof anObject);
+
+var anArray = new Array();
+console.log('value of anArray:', anArray);
+console.log('typeof anArray', typeof anArray);
+
+var aFunction = new Function();
+console.log('value of aFunction:', aFunction);
+console.log('typeof aFunction', typeof aFunction);
+
+var aNumber = new Number();
+console.log('value of aNumber:', aNumber);
+console.log('typeof aNumber', typeof aNumber);
+
+var aBoolean = new Boolean();
+console.log('value of aBoolean:', aBoolean);
+console.log('typeof aBoolean', typeof aBoolean);
