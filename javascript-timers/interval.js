391bab79c36521e3ffc3a5a5153522cb62df8b1c
Viviane Vieira <viviane.kodama@gmail.com>
2020-11-04 11:16:52 -0800

    Revised interval.js file.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,15 +1,12 @@
 var h1 = document.querySelector('h1');
 
 function countdown() {
-  if (h1.textContent === '4') {
-    h1.textContent = 3;
-  } else if (h1.textContent === '3') {
-    h1.textContent = 2;
-  } else if (h1.textContent === '2') {
-    h1.textContent = 1;
-  } else if (h1.textContent === '1') {
+  var newH1 = h1.textContent - 1;
+  h1.textContent = newH1;
+  if (h1.textContent === '0') {
     h1.textContent = '~Earth Beeeelooowww Us~';
-  } else if (h1.textContent === '~Earth Beeeelooowww Us~') {
+  }
+  if (h1.textContent === '~Earth Beeeelooowww Us~') {
     clearInterval(interval);
   }
 }
