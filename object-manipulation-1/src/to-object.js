7026b4b700f7a4040c8b59366977904d5011cd80
Viviane Vieira <viviane.kodama@gmail.com>
2020-11-07 18:16:14 -0800

    Revised exercise.

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 function toObject(keyValuePair) {
-  var key;
-  var value;
-  var object = { key: value };
-  key = keyValuePair[0];
-  value = keyValuePair[1];
-  return object;
+  var newObj = {};
+  newObj[keyValuePair[0]] = keyValuePair[1];
+  return newObj;
 }
