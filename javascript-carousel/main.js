31cf63495393971e82b3ff81c851f569a7fecf72
Viviane Vieira <viviane.kodama@gmail.com>
2020-11-19 18:05:04 -0800

    Revised main.js file

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -2,7 +2,25 @@ var index = 0;
 var $circles = document.querySelectorAll('.circles');
 var $images = document.querySelectorAll('.images');
 
-function updateCarousel() {
+function previous(index) {
+  var previousIndex;
+  previousIndex = index - 1;
+  if (index === 0) {
+    previousIndex = $images.length - 1;
+  }
+  return previousIndex;
+}
+
+function next(index) {
+  var nextIndex;
+  nextIndex = index + 1;
+  if (index === $images.length - 1) {
+    nextIndex = 0;
+  }
+  return nextIndex;
+}
+
+function update(index) {
   for (var i = 0; i < $images.length; i++) {
     $images[i].className = 'images hidden';
     $circles[i].className = 'far fa-circle circles';
@@ -11,77 +29,88 @@ function updateCarousel() {
       $circles[i].className = 'fas fa-circle circles';
     }
   }
-  index++;
-  if (index === $images.length) {
-    index = 0;
-  }
 }
 
-var carousel;
+// function updateCarousel() {
+//   for (var i = 0; i < $images.length; i++) {
+//     $images[i].className = 'images hidden';
+//     $circles[i].className = 'far fa-circle circles';
+//     if (i === index) {
+//       $images[i].className = 'images';
+//       $circles[i].className = 'fas fa-circle circles';
+//     }
+//   }
+//   index++;
+//   if (index === $images.length) {
+//     index = 0;
+//   }
+// }
 
-function callCarousel() {
-  carousel = setInterval(updateCarousel, 3000);
-}
-callCarousel();
+// var carousel;
 
-var $circlesCont = document.querySelector('#circles-cont');
+// function callCarousel() {
+//   carousel = setInterval(updateCarousel, 3000);
+// }
+// callCarousel();
 
-$circlesCont.addEventListener('click', function () {
-  clearInterval(carousel);
-  var circlesView = event.target.getAttribute('data-view');
-  for (var i = 0; i < $images.length; i++) {
-    var imagesView = $images[i].getAttribute('data-view');
-    $images[i].className = 'images hidden';
-    $circles[i].className = 'far fa-circle circles';
-    if (circlesView === imagesView) {
-      $images[i].className = 'images';
-      $circles[i].className = 'fas fa-circle circles';
-      index = i;
-    }
-  }
-  callCarousel();
-});
+// var $circlesCont = document.querySelector('#circles-cont');
 
-var $chevronLeft = document.querySelector('#chevron-left');
+// $circlesCont.addEventListener('click', function () {
+//   clearInterval(carousel);
+//   var circlesView = event.target.getAttribute('data-view');
+//   for (var i = 0; i < $images.length; i++) {
+//     var imagesView = $images[i].getAttribute('data-view');
+//     $images[i].className = 'images hidden';
+//     $circles[i].className = 'far fa-circle circles';
+//     if (circlesView === imagesView) {
+//       $images[i].className = 'images';
+//       $circles[i].className = 'fas fa-circle circles';
+//       index = i;
+//     }
+//   }
+//   callCarousel();
+// });
 
-$chevronLeft.addEventListener('click', function () {
-  clearInterval(carousel);
-  var indexChevron = index - 1;
-  for (var i = 0; i < $images.length; i++) {
-    $images[i].className = 'images hidden';
-    $circles[i].className = 'far fa-circle circles';
-    if (i === indexChevron) {
-      $images[i].className = 'images';
-      $circles[i].className = 'fas fa-circle circles';
-      index = i;
-    }
-    if (indexChevron === -1) {
-      $images[$images.length - 1].className = 'images';
-      $circles[$images.length - 1].className = 'fas fa-circle circles';
-      index = $images.length - 1;
-    }
-  }
-  callCarousel();
-});
+// var $chevronLeft = document.querySelector('#chevron-left');
 
-var $chevronRight = document.querySelector('#chevron-right');
+// $chevronLeft.addEventListener('click', function () {
+//   clearInterval(carousel);
+//   var indexChevron = index - 1;
+//   for (var i = 0; i < $images.length; i++) {
+//     $images[i].className = 'images hidden';
+//     $circles[i].className = 'far fa-circle circles';
+//     if (i === indexChevron) {
+//       $images[i].className = 'images';
+//       $circles[i].className = 'fas fa-circle circles';
+//       index = i;
+//     }
+//     if (indexChevron === -1) {
+//       $images[$images.length - 1].className = 'images';
+//       $circles[$images.length - 1].className = 'fas fa-circle circles';
+//       index = $images.length - 1;
+//     }
+//   }
+//   callCarousel();
+// });
 
-$chevronRight.addEventListener('click', function () {
-  clearInterval(carousel);
-  var indexChevron = index + 1;
-  for (var i = 0; i < $images.length; i++) {
-    $images[i].className = 'images hidden';
-    $circles[i].className = 'far fa-circle circles';
-    if (i === indexChevron) {
-      $images[i].className = 'images';
-      $circles[i].className = 'fas fa-circle circles';
-      index++;
-    }
-    if (indexChevron === $images.length) {
-      $images[0].className = 'images';
-      $circles[0].className = 'fas fa-circle circles';
-      index = 0;
-    }
-  }
-  callCarousel();
-});
+// var $chevronRight = document.querySelector('#chevron-right');
+
+// $chevronRight.addEventListener('click', function () {
+//   clearInterval(carousel);
+//   var indexChevron = index + 1;
+//   for (var i = 0; i < $images.length; i++) {
+//     $images[i].className = 'images hidden';
+//     $circles[i].className = 'far fa-circle circles';
+//     if (i === indexChevron) {
+//       $images[i].className = 'images';
+//       $circles[i].className = 'fas fa-circle circles';
+//       index++;
+//     }
+//     if (indexChevron === $images.length) {
+//       $images[0].className = 'images';
+//       $circles[0].className = 'fas fa-circle circles';
+//       index = 0;
+//     }
+//   }
+//   callCarousel();
+// });
