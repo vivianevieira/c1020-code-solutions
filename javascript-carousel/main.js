3720427da7565487b7c37b34bf8500f29234e018
Viviane Vieira <viviane.kodama@gmail.com>
2020-11-20 16:50:11 -0800

    Added an autoPlay function

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -21,6 +21,7 @@ function next(index) {
 }
 
 function update(index) {
+  clearTimeout(timerId);
   for (var i = 0; i < $images.length; i++) {
     $images[i].className = 'images hidden';
     $circles[i].className = 'far fa-circle circles';
@@ -29,6 +30,7 @@ function update(index) {
       $circles[i].className = 'fas fa-circle circles';
     }
   }
+  autoPlay();
 }
 
 var $chevronLeft = document.querySelector('#chevron-left');
@@ -53,86 +55,13 @@ $circlesCont.addEventListener('click', function () {
   update(index);
 });
 
-// function updateCarousel() {
-//   for (var i = 0; i < $images.length; i++) {
-//     $images[i].className = 'images hidden';
-//     $circles[i].className = 'far fa-circle circles';
-//     if (i === index) {
-//       $images[i].className = 'images';
-//       $circles[i].className = 'fas fa-circle circles';
-//     }
-//   }
-//   index++;
-//   if (index === $images.length) {
-//     index = 0;
-//   }
-// }
+var timerId;
 
-// var carousel;
+function autoPlay() {
+  timerId = setTimeout(function () {
+    index = next(index);
+    update(index);
+  }, 3000);
+}
 
-// function callCarousel() {
-//   carousel = setInterval(updateCarousel, 3000);
-// }
-// callCarousel();
-
-// var $circlesCont = document.querySelector('#circles-cont');
-
-// $circlesCont.addEventListener('click', function () {
-//   clearInterval(carousel);
-//   var circlesView = event.target.getAttribute('data-view');
-//   for (var i = 0; i < $images.length; i++) {
-//     var imagesView = $images[i].getAttribute('data-view');
-//     $images[i].className = 'images hidden';
-//     $circles[i].className = 'far fa-circle circles';
-//     if (circlesView === imagesView) {
-//       $images[i].className = 'images';
-//       $circles[i].className = 'fas fa-circle circles';
-//       index = i;
-//     }
-//   }
-//   callCarousel();
-// });
-
-// var $chevronLeft = document.querySelector('#chevron-left');
-
-// $chevronLeft.addEventListener('click', function () {
-//   clearInterval(carousel);
-//   var indexChevron = index - 1;
-//   for (var i = 0; i < $images.length; i++) {
-//     $images[i].className = 'images hidden';
-//     $circles[i].className = 'far fa-circle circles';
-//     if (i === indexChevron) {
-//       $images[i].className = 'images';
-//       $circles[i].className = 'fas fa-circle circles';
-//       index = i;
-//     }
-//     if (indexChevron === -1) {
-//       $images[$images.length - 1].className = 'images';
-//       $circles[$images.length - 1].className = 'fas fa-circle circles';
-//       index = $images.length - 1;
-//     }
-//   }
-//   callCarousel();
-// });
-
-// var $chevronRight = document.querySelector('#chevron-right');
-
-// $chevronRight.addEventListener('click', function () {
-//   clearInterval(carousel);
-//   var indexChevron = index + 1;
-//   for (var i = 0; i < $images.length; i++) {
-//     $images[i].className = 'images hidden';
-//     $circles[i].className = 'far fa-circle circles';
-//     if (i === indexChevron) {
-//       $images[i].className = 'images';
-//       $circles[i].className = 'fas fa-circle circles';
-//       index++;
-//     }
-//     if (indexChevron === $images.length) {
-//       $images[0].className = 'images';
-//       $circles[0].className = 'fas fa-circle circles';
-//       index = 0;
-//     }
-//   }
-//   callCarousel();
-// });
+autoPlay();
