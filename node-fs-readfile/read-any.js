a7c59facdc0543b4c77654f147976849832de949
Viviane Vieira <viviane.kodama@gmail.com>
2020-11-25 17:21:29 -0800

    node-fs-readfile exercise completed

---
+++ node-fs-readfile/read-any.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+
+const fileName = process.argv[2];
+
+fs.readFile(fileName, 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
