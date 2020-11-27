9ad635d1d5643bf8cb18624ba5661480061dfeae
Viviane Vieira <viviane.kodama@gmail.com>
2020-11-26 19:32:57 -0800

    node-fs-writefile modifications.

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -4,14 +4,14 @@ const readFile = process.argv[2];
 
 const writeFile = process.argv[3];
 
-function readContent(callback) {
+function copyContent(callback) {
   fs.readFile(readFile, 'utf8', (err, data) => {
     if (err) return callback(err);
     callback(null, data);
   });
 }
 
-readContent((err, data) => {
+copyContent((err, data) => {
   if (err) throw err;
   fs.writeFile(writeFile, data, 'utf8', err => {
     if (err) throw err;
