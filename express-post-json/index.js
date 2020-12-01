1a7742af0ed266e511de01a2855f1388dfcaf9c9
Viviane Vieira <viviane.kodama@gmail.com>
2020-12-01 13:34:51 -0800

    express-post-json completed.

---
+++ express-post-json/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+const grades = [];
+
+let nextId = 1;
+
+const jsonExpress = express.json();
+
+app.get('/api/grades', (req, res) => {
+  res.json(grades);
+});
+
+app.use(jsonExpress);
+
+app.post('/api/grades', (req, res) => {
+  const newGrade = req.body;
+  newGrade.id = nextId;
+  nextId++;
+  grades.push(newGrade);
+  res.json(newGrade);
+  res.status(201);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server listening on port 3000');
+});
