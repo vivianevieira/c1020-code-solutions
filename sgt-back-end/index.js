85e4eca9c649858882553e500373ed3e0f037ad1
Viviane Vieira <viviane.kodama@gmail.com>
2020-12-05 01:32:37 +0000

    Handled POST requests on path /api/grades.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 const pg = require('pg');
-
 const db = new pg.Pool({
   connectionString: 'postgres://dev:lfz@localhost/studentGradeTable'
 });
+
+const jasonMiddleware = express.json();
+app.use(jasonMiddleware);
 
 app.get('/api/grades', (req, res) => {
   db.query('select* from "grades"')
@@ -19,7 +21,30 @@ app.get('/api/grades', (req, res) => {
 });
 
 app.post('/api/grades', (req, res) => {
+  const sql = `
+  insert into "grades" ("name", "course", "score")
+          values ($1, $2, $3)
+          returning *
+  `;
+  const postContent = req.body;
+  const gradeScore = parseInt(postContent.score, 10);
+  const values = [postContent.name, postContent.course, gradeScore];
 
+  if (postContent.name === undefined || postContent.course === undefined || postContent.score === undefined) {
+    res.status(400).json({ error: 'Invalid grade entry' }); return;
+  } else if (!Number.isInteger(gradeScore) || gradeScore < 1 || gradeScore > 100) {
+    res.status(400).json({ error: 'grade must be a number from 1 to 100' });
+    return;
+  }
+
+  db.query(sql, values)
+    .then(result => {
+      res.status(201).json(result.rows[0]);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    });
 });
 
 app.listen(3000, () => {
