4a9c6a04e2a8af5c71d5c956733fac2e8b044e8c
Viviane Vieira <viviane.kodama@gmail.com>
2020-12-06 00:13:43 +0000

    Handleded delete requests on app. Setup SQL parameteres, queried the database and handled success and error.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -21,21 +21,33 @@ app.get('/api/grades', (req, res) => {
 });
 
 app.post('/api/grades', (req, res) => {
+  const bodyContent = req.body;
+  const gradeScore = parseInt(bodyContent.score, 10);
+
+  if (bodyContent.name === undefined) {
+    res.status(400).json({ error: 'name is required' });
+    return;
+  }
+  if (bodyContent.course === undefined) {
+    res.status(400).json({ error: 'course is required' });
+    return;
+  }
+  if (bodyContent.score === undefined) {
+    res.status(400).json({ error: 'score is required' });
+    return;
+  }
+  if (!Number.isInteger(gradeScore) || gradeScore < 1 || gradeScore > 100) {
+    res.status(400).json({ error: 'grade must be a number from 1 to 100' });
+    return;
+  }
+
   const sql = `
   insert into "grades" ("name", "course", "score")
           values ($1, $2, $3)
           returning *
   `;
-  const postContent = req.body;
-  const gradeScore = parseInt(postContent.score, 10);
-  const values = [postContent.name, postContent.course, gradeScore];
 
-  if (postContent.name === undefined || postContent.course === undefined || postContent.score === undefined) {
-    res.status(400).json({ error: 'Invalid grade entry' }); return;
-  } else if (!Number.isInteger(gradeScore) || gradeScore < 1 || gradeScore > 100) {
-    res.status(400).json({ error: 'grade must be a number from 1 to 100' });
-    return;
-  }
+  const values = [bodyContent.name, bodyContent.course, gradeScore];
 
   db.query(sql, values)
     .then(result => {
@@ -51,22 +63,25 @@ app.put('/api/grades/:gradeId', (req, res) => {
   const gradeId = parseInt(req.params.gradeId, 10);
   const bodyContent = req.body;
   const gradeScore = parseInt(bodyContent.score, 10);
-  const values = [bodyContent.name, bodyContent.course, gradeScore, gradeId];
 
   if (!Number.isInteger(gradeId) || gradeId <= 0) {
-    res.status(400).json({
-      error: 'gradeId must be a positive integer'
-    });
+    res.status(400).json({ error: 'gradeId must be a positive integer' });
     return;
   }
-  if (bodyContent.name === undefined || bodyContent.course === undefined || bodyContent.score === undefined) {
-    res.status(400).json({
-      error: 'invalid grade entry'
-    });
+  if (bodyContent.name === undefined) {
+    res.status(400).json({ error: 'name is required' });
+    return;
+  }
+  if (bodyContent.course === undefined) {
+    res.status(400).json({ error: 'course is required' });
+    return;
+  }
+  if (bodyContent.score === undefined) {
+    res.status(400).json({ error: 'score is required' });
     return;
   }
   if (!Number.isInteger(gradeScore) || gradeScore < 1 || gradeScore > 100) {
-    res.status(400).json({ error: 'grade must be a number from 1 to 100' });
+    res.status(400).json({ error: 'score must be a number from 1 to 100' });
     return;
   }
 
@@ -76,11 +91,14 @@ app.put('/api/grades/:gradeId', (req, res) => {
         "course" = $2,
         "score" = $3
   where "gradeId" = $4
+  returning *
   `;
+
+  const values = [bodyContent.name, bodyContent.course, gradeScore, gradeId];
 
   db.query(sql, values)
     .then(result => {
-      const grade = result.rows;
+      const grade = result.rows[0];
       if (!grade) {
         res.status(404).json({ error: `Cannot find grade with gradeId ${gradeId}` });
       } else {
@@ -93,6 +111,41 @@ app.put('/api/grades/:gradeId', (req, res) => {
     });
 });
 
+app.delete('/api/grades/:gradeId', (req, res) => {
+  const gradeId = parseInt(req.params.gradeId, 10);
+
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({
+      error: 'gradeId must be a positive integer'
+    });
+    return;
+  }
+
+  const sql = `
+  delete from "grades"
+  where "gradeId" = $1
+  returning *
+  `;
+
+  const values = [gradeId];
+
+  db.query(sql, values)
+    .then(result => {
+      const grade = result.rows[0];
+      if (!grade) {
+        res.status(404).json({ error: `Cannot find grade with gradeId ${gradeId}` });
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    });
+
+});
+
 app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('Server is running on port 3000');
 });
