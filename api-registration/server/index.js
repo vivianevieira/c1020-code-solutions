a5d0daacc1da7e2dc1b7190d158c397245060170
Viviane Vieira <viviane.kodama@gmail.com>
2020-12-16 18:21:00 -0800

    Revised index.js file with corrections.

--- api-registration/server/index.js
+++ api-registration/server/index.js
@@ -23,7 +23,7 @@ app.post('/api/auth/sign-up', (req, res, next) => {
   const sql = `
     insert into "users" ("username", "hashedPassword")
     values ($1, $2)
-    returning *
+    returning "userId", "username", "createdAt"
   `;
   argon2
     .hash(password)
@@ -32,7 +32,7 @@ app.post('/api/auth/sign-up', (req, res, next) => {
 
       db.query(sql, params)
         .then(result => {
-          const newUser = result.rows;
+          const [newUser] = result.rows;
           res.status(201).json(newUser);
         })
         .catch(err => next(err));
